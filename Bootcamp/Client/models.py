28e0174aa17c19880b6dc28f2ff2fd011b89eaf9
BlueLuscious <lucio.200152@gmail.com>
2023-08-21 02:42:46 -0300

    Create Client App

---
+++ Bootcamp/Client/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class WorkLineChoices(models.TextChoices):
+    BAKER = "Baker"
+    BEAT_MAKER = "Beat Maker"
+    JEWELER = "Jeweler"
+    LAWYER = "Lawyer"
+    STRIPPER = "Stripper"
+    IS_NONE = "None"
+    
+class ClientModel(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=256)
+    password = models.CharField(max_length=50)
+    cuit = models.CharField(max_length=11)
+    work_line = models.CharField(choices=WorkLineChoices.choices, max_length=256, default=WorkLineChoices.IS_NONE)
+    interest = models.CharField(max_length=256)
+    is_seller = models.BooleanField(default=False)
+    is_buyer = models.BooleanField(default=True)
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=150)
+    area_code = models.CharField(max_length=6)
+    
