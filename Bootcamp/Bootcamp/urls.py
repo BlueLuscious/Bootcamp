a96001a13d3fc1050d2309b5a5361329cdfd70b4
BlueLuscious <lucio.200152@gmail.com>
2023-08-22 21:50:43 -0300

    update urls.py (Bootcamp)
    include App's urls

--- Bootcamp/Bootcamp/urls.py
+++ Bootcamp/Bootcamp/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Client.urls')),
+    path('', include('SignUp.urls')),
+    path('', include('Login.urls')),
 ]
