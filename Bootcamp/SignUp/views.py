c894707e12fa138fc900448be8df4fa86bae514a
BlueLuscious <lucio.200152@gmail.com>
2023-08-22 21:58:23 -0300

    update views.py (SignUp)

--- Bootcamp/SignUp/views.py
+++ Bootcamp/SignUp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
